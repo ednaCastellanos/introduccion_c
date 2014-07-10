cbfa0289d5113db3034de9f9d30d486b90a58723
edna castellanos <edniuux@gmail.com>
2014-07-09 20:02:01 -0500

    Primeros programas en c. *Ejemplos básicos*

---
+++ suma.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int a,b,c;
+	printf("Valor de a: \n");
+	scanf("%d", &a);
+	printf("Valor de b: \n");
+	scanf("%d", &b);	
+	printf("La suma es:%d", a + b);
+	return 0;
+}
